c23fef990f6c139d2b33c23231fcfff3ff1c7c53
K.Nishi <24signals@gmail.com>
2011-02-21 03:08:34 +0900

    igo-rubyを使った形態素解析機能の内包を削除した。

--- test/test.rb
+++ test/test.rb
@@ -3,8 +3,6 @@
 #Copyright:: Copyright (c) kyow, 2011
 #Authors:: K.Nishi
 
-#require '../lib/tfidf_ja'
-#require '../lib/tfidf_ja/igo_extension'
 require 'rubygems'
 require 'tfidf_ja'
 require 'tfidf_ja/igo_extension'
@@ -16,19 +14,11 @@ def output(h)
 end
 
 tfidf = TfIdf::Ja.new
+puts "-->1回目"
 output tfidf.tfidf(['この', '文章', 'から', 'TFIDF', '値', 'を', '取得', 'する'])
+puts "-->2回目"
 output tfidf.tfidf(['文章', 'を', '連続', 'で', '渡す', 'と', 'TFIDF', '値', 'を', '追加', 'で', '算出', 'する'])
 
-
-# 以下はigo-rubyが必要
-dir = '../../ipadic'
-tie = TfIdf::IgoExtension.new(dir)
-puts s = "この文字列を形態素解析してTFIDF値を取得する"
-output tie.tfidf(s)
-
-puts s = "連続してインスタンスを使うと前回解析したTFIDF計算結果を加味する"
-output tie.tfidf(s)
-
-tie.reset
-puts s = "TfIdf::IgoExtension.resetでリセットする"
-output tie.tfidf(s)
+puts "-->リセット"
+tfidf.reset
+output tfidf.tfidf(['TFIDF', '#', 'rest', 'で', 'リセット', 'する'])
