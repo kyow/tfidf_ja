d1ae45055f78467ae679fa64ab8a82c2b8d8f362
yuuki arisawa <yuuki.ari@gmail.com>
2011-06-23 22:26:44 +0900

    サンプルに出ている例のテストを追加

--- spec/tfidf_ja_spec.rb
+++ spec/tfidf_ja_spec.rb
@@ -1,7 +1,29 @@
+# -*- encoding: utf-8 -*-
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/../lib/tfidf_ja')
 
-describe "TfidfJa" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
-  end
+describe TfIdf::Ja do
+  subject { TfIdf::Ja.new.instance_variable_get(:@idfs) }
+  it {
+    subject.exists?("この").should == true
+    subject.exists?("文章").should == true
+    subject.exists?("から").should == true
+    subject.exists?("値").should   == true
+    subject.exists?("を").should   == true
+    subject.exists?("取得").should == true
+    subject.exists?("する").should == true
+  }
+  it {
+    subject.get("この").should_not == nil
+    subject.get("文章").should_not == nil
+    subject.get("から").should_not == nil
+    subject.get("値").should_not   == nil
+    subject.get("を").should_not   == nil
+    subject.get("取得").should_not == nil
+    subject.get("する").should_not == nil
+  }
+  it {
+    subject.exists?("TFIDF").should == false
+    subject.get("TFIDF").should     == nil
+  }
 end
